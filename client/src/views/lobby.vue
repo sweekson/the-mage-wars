190f5c50ca8a259d6f5092c317c7a62e24de19ca
Wilson Su <sweekson@gmail.com>
2021-09-23 02:58:46 +0900

    feat: display online players on the lobby

--- client/src/views/lobby.vue
+++ client/src/views/lobby.vue
@@ -1,9 +1,11 @@
 <script>
+import Players from '@components/players';
 import BadRequest from '../errors/bad-request.vue';
 
 export default {
   inject: ['auth', 'room'],
   components: {
+    Players,
     BadRequest,
   },
 }
@@ -12,6 +14,8 @@ export default {
 <template>
   <section v-if="auth.isLoggedIn && room.isLoaded && !room.isJoined">
     <h2>Lobby</h2>
+
+    <h3>Rooms</h3>
     <form class="flexbox">
       <input v-model="room.name" type="text" class="flex-1" />
       <button type="button" @click="room.onCreate">Create</button>
@@ -24,6 +28,8 @@ export default {
       <span class="flex-1">{{ item.name }} ({{ item.admin.name }}) ({{ item.status }}) ({{ item.players.length }}) </span>
       <button v-if="item.isOpen" type="button" @click="room.onJoin(item.id)">Join</button>
     </div>
+    <h3>Online Players</h3>
+    <Players :list="room.players" />
   </section>
   <BadRequest v-else />
 </template>
