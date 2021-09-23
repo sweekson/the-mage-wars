19829344c74177b394dcde5b286041a403345024
Wilson Su <sweekson@gmail.com>
2021-09-23 18:32:44 +0900

    core: update the implementation of 'makeError'

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,14 +1,18 @@
+import { convert } from '@feathersjs/errors';
+
 import { ServiceResult } from '../declarations';
 
-export const makeResult = (type: string, detail: any): ServiceResult => {
-  return { type, ok: true, detail };
+export const makeResult = (
+  type: string, detail: any,
+): Promise<ServiceResult> => {
+  return Promise.resolve({ type, ok: true, detail });
 };
 
 export const makeError = (
   type: string,
   code: number,
   message: string,
-  detail?: any
-): ServiceResult => {
-  return { type, ok: false, error: { code, message }, detail };
+  data: any = null,
+): Promise<ServiceResult> => {
+  return Promise.reject(convert({ name: code, message, data }));
 };
