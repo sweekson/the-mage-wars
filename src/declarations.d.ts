2168764a5fa9c3090658990b17238ca873aa8a20
Wilson Su <sweekson@gmail.com>
2021-09-23 18:43:31 +0900

    chore: update type declarations

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -5,14 +5,8 @@ export interface ServiceTypes {}
 // The application instance type that will be used everywhere else
 export type Application = ExpressFeathers<ServiceTypes>;
 
-export interface ServiceError {
-  code: number;
-  message: string;
-}
-
 export interface ServiceResult {
   type: string;
   ok: boolean;
   detail?: any;
-  error?: ServiceError;
 }
