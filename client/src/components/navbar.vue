d014e44c6ad4fcfe9587517ebc8185d43852dfe3
Wilson Su <sweekson@gmail.com>
2021-09-23 22:47:07 +0900

    feat: add function for changing locale

--- client/src/components/navbar.vue
+++ client/src/components/navbar.vue
@@ -1,12 +1,32 @@
 <script>
 export default {
   inject: ['auth'],
+  computed: {
+    langs() {
+      return [
+        { locale: 'en', text: 'English' },
+        { locale: 'ja', text: '日本語' },
+      ];
+    },
+  },
 }
 </script>
 
 <template>
   <nav class="flexbox flexbox-center-y">
     <span class="flex-1">{{ auth.current.name }}</span>
+
+    <template v-for="lang in langs">
+      <button
+        v-if="$i18n.locale !== lang.locale"
+        v-html="lang.text"
+        :key="lang.locale"
+        type="button"
+        class="lang"
+        @click="() => $i18n.locale = lang.locale"
+      />
+    </template>
+
     <button v-t="'common.logout'" type="button" @click="auth.onLogout"></button>
   </nav>
 </template>
@@ -18,5 +38,12 @@ nav {
   width: 100%;
   height: 30px;
   padding: 4px;
+}
+.lang {
+  background-color: transparent;
+  border: 0;
+  cursor: pointer;
+  color: #fff;
+  padding: 0 8px;
 }
 </style>
