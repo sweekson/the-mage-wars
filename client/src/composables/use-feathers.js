e087c5c167c09ddaaaf736209169bf7827ed61aa
Wilson Su <sweekson@gmail.com>
2021-09-24 21:53:41 +0900

    feat(client): add keep alive function

--- client/src/composables/use-feathers.js
+++ client/src/composables/use-feathers.js
@@ -1,7 +1,32 @@
+import { ref, watch } from 'vue';
 import io from 'socket.io-client';
 import feathers from '@feathersjs/feathers';
 import socketio from '@feathersjs/socketio-client';
 import authentication from '@feathersjs/authentication-client';
+
+export const useKeepAlive = ({ socket }) => {
+  const timeout = ref(30000);
+  const focused = ref(true);
+  const enabled = ref(false);
+  const enable = () => (enabled.value = true);
+  const disable = () => (enabled.value = false);
+  const ping = () => {
+    setTimeout(() => {
+      socket.emit('ping');
+      focused.value && enabled.value && ping();
+    }, timeout.value);
+  };
+
+  watch([focused, enabled], ([x1, x2]) => x1 && x2 && ping());
+
+  window.addEventListener('focus', () => (focused.value = true));
+  window.addEventListener('blur', () => (focused.value = false));
+
+  return {
+    enable,
+    disable,
+  };
+};
 
 export const useFeathers = ({ host }) => {
   const socket = io(host, {
@@ -9,6 +34,9 @@ export const useFeathers = ({ host }) => {
     forceNew: true
   });
   const client = feathers();
+  const keep = useKeepAlive({ socket });
+
+  keep.enable();
 
   client.configure(socketio(socket));
   client.configure(authentication());
