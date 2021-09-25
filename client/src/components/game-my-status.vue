68ef7f925e8b907e0b94c8c583de897c6b4edb54
Wilson Su <sweekson@gmail.com>
2021-09-25 23:00:46 +0900

    feat(client): show my attributes on the game status

--- client/src/components/game-my-status.vue
+++ client/src/components/game-my-status.vue
@@ -1,17 +1,29 @@
 <script>
+import Flexbox from '@components/flexbox';
+
 export default {
   inject: ['game'],
+  components: {
+    Flexbox,
+  },
 }
 </script>
 
 <template>
-  <section class="my-status">
+  <Flexbox class="my-status align-items-start">
     <div class="team">{{ game.me.team }}</div>
-  </section>
+    <Flexbox class="attribute">
+      <span class="flex-1">Strength</span> {{ game.me.strength }}
+    </Flexbox>
+    <Flexbox class="attribute">
+      <span class="flex-1">Defense</span> {{ game.me.defense }}
+    </Flexbox>
+  </Flexbox>
 </template>
 
 <style lang="scss" scoped>
 .my-status {
+  justify-content: flex-start;
   margin-bottom: 12px;
 }
 .team {
@@ -23,5 +35,16 @@ export default {
   display: inline-block;
   width: 20px;
   height: 20px;
+}
+.attribute {
+  background-color: #666;
+  border-radius: 10px;
+  font-weight: 300;
+  line-height: 20px;
+  color: #fff;
+  width: 100px;
+  height: 20px;
+  margin-left: 4px;
+  padding: 2px 8px;
 }
 </style>
