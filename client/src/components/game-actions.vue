2de9d70933927a5fe603e8742e388219cfa4bfc7
Wilson Su <sweekson@gmail.com>
2021-09-25 21:02:31 +0900

    chore: update button styles on game component

--- client/src/components/game-actions.vue
+++ client/src/components/game-actions.vue
@@ -8,46 +8,41 @@ export default {
   <section>
     <button
       :disabled="!game.action.isPrayable"
-      class="btn btn-block"
+      class="btn btn-block btn-skew"
       type="button"
+      data-text="Pray"
       @click="game.action.onPray"
-    >
-      Pray
-    </button>
+    />
 
     <button
       :disabled="!game.action.isExchangeable"
-      class="btn btn-block"
+      class="btn btn-block btn-skew"
       type="button"
+      data-text="Exchange"
       @click="game.action.onExchange"
-    >
-      Exchange
-    </button>
+    />
 
     <button
       :disabled="!game.action.isCastable"
-      class="btn btn-block"
+      class="btn btn-block btn-skew"
       type="button"
+      :data-text="`Cast Spells (${game.me.actions})`"
       @click="game.action.onCast"
-    >
-      Cast Spells ({{ game.me.actions }})
-    </button>
+    />
 
     <button
       :disabled="!game.action.isPassable"
-      class="btn btn-block"
+      class="btn btn-block btn-skew"
       type="button"
+      data-text="Pass"
       @click="game.action.onPass"
-    >
-      Pass
-    </button>
+    />
 
     <button
-      class="btn btn-block"
+      class="btn btn-block btn-skew"
       type="button"
+      data-text="Leave"
       @click="room.onLeave"
-    >
-      Leave
-    </button>
+    />
   </section>
 </template>
