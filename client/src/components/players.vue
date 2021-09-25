e957f13d7c060b4676273031656d7776e3372377
Wilson Su <sweekson@gmail.com>
2021-09-25 19:08:40 +0900

    chore: update styles of player list

--- client/src/components/players.vue
+++ client/src/components/players.vue
@@ -1,5 +1,10 @@
 <script>
+import Flexbox from '@components/flexbox';
+
 export default {
+  components: {
+    Flexbox,
+  },
   props: {
     list: {
       type: Array,
@@ -10,12 +15,27 @@ export default {
 </script>
 
 <template>
-  <section>
+  <Flexbox class="players">
     <div
       v-for="player in list"
       :key="player.uid"
+      class="player"
     >
       {{ player.name }}
     </div>
-  </section>
+  </Flexbox>
 </template>
+
+<style lang="scss" scoped>
+.players {
+  flex-wrap: wrap;
+  gap: 4px;
+}
+.player {
+  background-color: #f6f6f6;
+  border: 1px solid #999;
+  border-radius: 2px;
+  width: 100%;
+  padding: 4px;
+}
+</style>
