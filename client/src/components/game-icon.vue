a4df5d1dd8bc7bf9448be27a9345e53272441688
Wilson Su <sweekson@gmail.com>
2021-09-26 16:00:22 +0900

    feat: add game icons and component

---
+++ client/src/components/game-icon.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    color: {
+      type: String,
+      default: 'gray',
+    },
+    size: {
+      type: String,
+      default: 'md',
+    },
+    shape: {
+      type: String,
+      default: 'square',
+    },
+  },
+};
+</script>
+
+
+<template>
+  <span
+    :class="[
+      'game-icon-item',
+      'game-icon-' + shape,
+    ]"
+  >
+    <span
+      :class="[
+        'game-icon',
+        'game-icon-' + name,
+        'game-icon-' + size,
+        'game-icon-color-' + color,
+      ]"
+    />
+  </span>
+</template>
