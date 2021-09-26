7a6f1346f9dcd04a97508f8142c473ff503e7a61
Wilson Su <sweekson@gmail.com>
2021-09-26 19:30:38 +0900

    chore(client): update styles of my game status

--- client/src/components/game-my-status.vue
+++ client/src/components/game-my-status.vue
@@ -1,50 +1,40 @@
 <script>
-import Flexbox from '@components/flexbox';
+import { NGrid, NGridItem, NStatistic } from 'naive-ui';
+
+import Hexagon from '@components/hexagon';
 
 export default {
   inject: ['game'],
   components: {
-    Flexbox,
+    NGrid,
+    NGridItem,
+    NStatistic,
+    Hexagon,
   },
 }
 </script>
 
 <template>
-  <flexbox class="my-status align-items-start">
-    <div class="team">{{ game.me.team }}</div>
-    <flexbox class="attribute">
-      <span class="flex-1">Strength</span> {{ game.me.strength }}
-    </flexbox>
-    <flexbox class="attribute">
-      <span class="flex-1">Defense</span> {{ game.me.defense }}
-    </flexbox>
-  </flexbox>
+  <n-grid cols="3 480:8 600:10 720:12" :x-gap="4">
+    <n-grid-item>
+      <hexagon height="60">
+        <span class="team">{{ game.me.team }}</span>
+      </hexagon>
+    </n-grid-item>
+
+    <n-grid-item>
+      <n-statistic label="Strengths">{{ game.me.strength }}</n-statistic>
+    </n-grid-item>
+
+    <n-grid-item>
+      <n-statistic label="Defense">{{ game.me.defense }}</n-statistic>
+    </n-grid-item>
+  </n-grid>
 </template>
 
 <style lang="scss" scoped>
-.my-status {
-  justify-content: flex-start;
-  margin-bottom: 12px;
-}
 .team {
-  background-color: #333;
-  border-radius: 10px;
-  line-height: 20px;
-  text-align: center;
-  color: #fff;
-  display: inline-block;
-  width: 20px;
-  height: 20px;
-}
-.attribute {
-  background-color: #666;
-  border-radius: 10px;
+  font-size: 40px;
   font-weight: 300;
-  line-height: 20px;
-  color: #fff;
-  width: 100px;
-  height: 20px;
-  margin-left: 4px;
-  padding: 2px 8px;
 }
 </style>
