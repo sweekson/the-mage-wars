87d5a3bf91c74795245f7879fc4096e7352351bd
Wilson Su <sweekson@gmail.com>
2021-09-26 22:37:08 +0900

    chore(game): update styles of the team number

--- client/src/components/game-my-status.vue
+++ client/src/components/game-my-status.vue
@@ -2,6 +2,7 @@
 import { NGrid, NGridItem, NStatistic } from 'naive-ui';
 
 import Hexagon from '@components/hexagon';
+import Flexbox from '@components/flexbox';
 
 export default {
   inject: ['game'],
@@ -10,6 +11,7 @@ export default {
     NGridItem,
     NStatistic,
     Hexagon,
+    Flexbox,
   },
 }
 </script>
@@ -18,7 +20,10 @@ export default {
   <n-grid cols="3 480:8 600:10 720:12" :x-gap="4">
     <n-grid-item>
       <hexagon height="60">
-        <span class="team">{{ game.me.team }}</span>
+        <flexbox column>
+          <h6 class="team-title">Team</h6>
+          <span class="team-number">{{ game.me.team }}</span>
+        </flexbox>
       </hexagon>
     </n-grid-item>
 
@@ -33,8 +38,14 @@ export default {
 </template>
 
 <style lang="scss" scoped>
-.team {
-  font-size: 40px;
-  font-weight: 300;
+.team-title {
+  font-size: 13px;
+  font-weight: 500;
+  color: #999;
+}
+.team-number {
+  font-size: 32px;
+  font-weight: 400;
+  line-height: 32px;
 }
 </style>
