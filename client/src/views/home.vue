69c1da156f91792d0f9035e37b9758e18166c027
Wilson Su <sweekson@gmail.com>
2021-09-28 01:30:28 +0900

    feat(home): refactor using Naive components

--- client/src/views/home.vue
+++ client/src/views/home.vue
@@ -1,29 +1,51 @@
 <script>
-import Flexbox from '@components/flexbox.vue';
+import { NSpace, NButton } from 'naive-ui';
 
 export default {
   components: {
-    Flexbox,
+    NSpace,
+    NButton,
   },
 }
 </script>
 
 <template>
-  <flexbox column full>
-    <h1>Welcome</h1>
-    <div>
-      <button
-        v-html="$t('common.signup')"
-        type="button"
-        class="btn"
-        @click="$router.push('/signup')"
-      />
-      <button
-        v-html="$t('common.login')"
-        type="button"
-        class="btn"
-        @click="$router.push('/login')"
-      />
-    </div>
-  </flexbox>
+  <n-space align="start" justify="center" class="fullscreen">
+    <n-space vertical align="center" justify="start" class="home">
+      <h1 class="home-title">Welcome</h1>
+      <small class="home-subtitle">
+        Let's play the board game online together!
+      </small>
+
+      <n-space>
+        <n-button
+          type="info"
+          @click="$router.push('/signup')"
+        >
+          {{ $t('common.signup') }}
+        </n-button>
+
+        <n-button
+          type="primary"
+          @click="$router.push('/login')"
+        >
+          {{ $t('common.login') }}
+        </n-button>
+      </n-space>
+    </n-space>
+  </n-space>
 </template>
+
+<style lang="scss" scoped>
+.home {
+  margin-top: 140px;
+}
+.home-title {
+  font-size: 4em;
+}
+.home-subtitle {
+  font-size: 1.5em;
+  font-weight: 200;
+  color: #999;
+}
+</style>
