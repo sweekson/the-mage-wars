92649f536331e6541b6937d92fc1cc003b21feb0
Wilson Su <sweekson@gmail.com>
2021-09-29 23:15:00 +0900

    feat(game): add draggable and collapsible for tile detail panel

--- client/src/components/game-tile-detail.vue
+++ client/src/components/game-tile-detail.vue
@@ -1,11 +1,23 @@
 <script>
+import { reactive } from 'vue';
+import { NIcon } from 'naive-ui';
+import { KeyboardArrowDownOutlined, KeyboardArrowUpOutlined } from '@vicons/material';
+
 import Flexbox from '@components/flexbox.vue';
 import GameTile from '@components/game-tile.vue';
+import { draggable } from '@directives/draggable';
+import { useCollapsible } from '@composables/use-collapsible';
 
 export default {
   components: {
+    NIcon,
+    KeyboardArrowDownOutlined,
+    KeyboardArrowUpOutlined,
     Flexbox,
     GameTile,
+  },
+  directives: {
+    draggable,
   },
   props: {
     data: {
@@ -13,23 +25,38 @@ export default {
       default: () => null,
     },
   },
+  setup() {
+    return {
+      collapsible: reactive(useCollapsible({ value: true })),
+    };
+  },
 }
 </script>
 <template>
-  <div class="tile-detail">
+  <div
+    v-draggable="{ handle: '#tile-detail-title' }"
+    :class="['tile-detail', { expanded: collapsible.expanded }]"
+  >
+    <h6 id="tile-detail-title" class="tile-detail-title flexbox flexbox-center-y">
+      <span class="flex-1">Tile Detail</span>
+
+      <n-icon size="20" color="#ccc" @click="collapsible.toggle">
+        <keyboard-arrow-up-outlined v-if="collapsible.expanded" />
+        <keyboard-arrow-down-outlined v-else />
+      </n-icon>
+    </h6>
+
     <flexbox v-if="!data" class="tile-detail-placeholder">
       Select a tile to see the detail
     </flexbox>
 
-    <game-tile
-      v-if="data"
-      :data="data"
-      size="lg"
-      standalone
-    />
-
-    <div class="tile-detail-info">
-
+    <div class="tile-detail-content">
+      <game-tile
+        v-if="data"
+        :data="data"
+        size="lg"
+        standalone
+      />
     </div>
   </div>
 </template>
@@ -37,14 +64,58 @@ export default {
 <style lang="scss" scoped>
 .tile-detail {
   background-color: #444;
-  width: 288px;
-  height: 144px;
-  margin-bottom: 8px;
+  border: 1px solid #666;
+  border-radius: 2px;
+  width: 200px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 400;
+
+  @include media-breakpoint-down(lg) {
+    transform: translate(calc(100vw - 208px), 260px);
+  }
+
+  @include media-breakpoint-up(lg) {
+    transform: translate(calc(100vw - 530px), 118px);
+  }
+
+  &.expanded {
+    height: 300px;
+  }
+
+  &-title {
+    background-color: #333;
+    border-radius: 2px;
+    cursor: move;
+    font-size: 14px;
+    font-weight: 400;
+    user-select: none;
+    line-height: 16px;
+    height: 32px;
+    padding: 8px;
+  }
+  &.expanded &-title {
+    border-bottom-right-radius: 0;
+    border-bottom-left-radius: 0;
+  }
 
   &-placeholder {
     font-weight: 300;
     color: #999;
-    height: 100%;
+    display: none;
+    height: calc(100% - 32px);
+  }
+  &.expanded &-placeholder {
+    display: flex;
+  }
+
+  &-content {
+    display: none;
+    padding: 8px;
+  }
+  &.expanded &-content {
+    display: block;
   }
 }
 </style>
