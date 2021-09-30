12d1e59f45ec255c97b220cb34b5927fc388afd5
Wilson Su <sweekson@gmail.com>
2021-09-30 23:40:48 +0900

    feat(lobby): update layout and styles

--- client/src/views/lobby.vue
+++ client/src/views/lobby.vue
@@ -1,5 +1,8 @@
 <script>
-import { NGrid, NGridItem, NInputGroup, NInput, NButton } from 'naive-ui';
+import {
+  NLayout, NLayoutHeader, NLayoutSider, NLayoutContent,
+  NSpace, NInputGroup, NInput, NButton,
+} from 'naive-ui';
 
 import Rooms from '@components/rooms';
 import Players from '@components/players';
@@ -9,8 +12,11 @@ import Loader from '@components/loader';
 export default {
   inject: ['auth', 'room'],
   components: {
-    NGrid,
-    NGridItem,
+    NLayout,
+    NLayoutHeader,
+    NLayoutSider,
+    NLayoutContent,
+    NSpace,
     NInputGroup,
     NInput,
     NButton,
@@ -24,32 +30,58 @@ export default {
 
 <template>
   <section v-if="auth.isLoggedIn && room.isLoaded && !room.isJoined">
-    <h2 v-html="$t('game.lobby')"></h2>
+    <n-layout content-style="padding: 8px;" class="transparent">
+      <n-layout-header class="header transparent">
+        <h2 v-html="$t('game.lobby')"></h2>
+      </n-layout-header>
 
-    <h3>Rooms</h3>
-    <form class="flexbox">
-      <n-grid cols="2 1080:3 1440:4">
-        <n-grid-item>
-          <n-input-group>
-            <n-input
-              v-model:value="room.name"
-              type="text"
-              placeholder="Enter room title"
-              class="flex-1"
-            />
-            <n-button
-              :disabled="!room.name"
-              @click="room.onCreate"
-            >
-              {{ $t('game.create') }}
-            </n-button>
-          </n-input-group>
-        </n-grid-item>
-      </n-grid>
-    </form>
-    <rooms />
-    <h3>Online Players</h3>
-    <players :list="room.players" />
+      <n-layout has-sider sider-placement="right" class="transparent">
+        <n-layout-content content-style="padding-right: 8px;" class="transparent">
+          <h3>Rooms</h3>
+          <flexbox v-if="!room.rooms.length" class="panel panel-empty empty-rooms">
+            No rooms have been created
+          </flexbox>
+          <rooms />
+
+          <h3>Online Players</h3>
+          <players :list="room.players" />
+        </n-layout-content>
+
+        <n-layout-sider :width="240" class="transparent">
+          <form class="flexbox panel panel-ghost">
+            <n-space vertical style="width: 100%;">
+              <h3>Create Room</h3>
+
+              <n-input
+                v-model:value="room.name"
+                type="text"
+                placeholder="Enter room title"
+                size="large"
+                class="flex-1"
+              />
+              <n-button
+                :disabled="!room.name"
+                size="large"
+                block
+                @click="room.onCreate"
+              >
+                {{ $t('game.create') }}
+              </n-button>
+            </n-space>
+          </form>
+        </n-layout-sider>
+      </n-layout>
+    </n-layout>
   </section>
   <flexbox v-else fullscreen><loader /></flexbox>
 </template>
+
+<style lang="scss" scoped>
+.transparent {
+  --color: transparent !important;
+}
+.empty-rooms {
+  width: 300px;
+  height: 100px;
+}
+</style>
