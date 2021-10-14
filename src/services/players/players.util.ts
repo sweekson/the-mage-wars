8bd6ea08917784bb23de31b3fd415c4b303f8012
Wilson Su <sweekson@gmail.com>
2021-10-14 12:45:26 +0900

    chore: add function return type

--- src/services/players/players.util.ts
+++ src/services/players/players.util.ts
@@ -2,8 +2,7 @@ import pick from 'lodash/pick';
 
 import { GamePlayer } from './players.class';
 
-export const toPlayerJSON = (player: GamePlayer) => {
+export const toPlayerJSON = (player: GamePlayer): Partial<GamePlayer> => {
   const buffs = new Array(player.buffs.length).fill(1);
   return { ...pick(player, ['uid', 'name', 'color']), buffs };
 };
-
