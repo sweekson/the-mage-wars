882a7ea70037f73708e8cb35099a0f491d3c94be
Wilson Su <sweekson@gmail.com>
2021-10-16 23:36:50 +0900

    chore(game): create shared component for camp status

---
+++ client/src/components/game-camp-status.vue
@@ -0,0 +1,70 @@
+<script>
+import { NProgress } from 'naive-ui';
+
+import GameCampBanner from '@components/game-camp-banner.vue';
+
+export default {
+  components: {
+    NProgress,
+    GameCampBanner,
+  },
+  props: {
+    number: {
+      type: String,
+      required: true,
+    },
+    energy: {
+      type: Number,
+      required: true,
+    },
+  },
+};
+</script>
+
+<template>
+  <h4 class="camp-title">
+    Camp {{ number }}
+  </h4>
+  <game-camp-banner
+    :color="!energy ? 'gray' : (number === '1' ? 'red' : 'navy')"
+  />
+  <n-progress
+    :percentage="energy"
+    :data-value="energy"
+    :show-indicator="false"
+    type="line"
+    color="#fa4"
+    :height="8"
+    :class="[
+      'camp-energy',
+      { 'camp-energy-zero': !energy },
+    ]"
+  />
+</template>
+
+<style lang="scss" scoped>
+.camp-title {
+  font-size: 20px;
+  font-weight: 200;
+  text-align: center;
+  margin-bottom: 12px;
+}
+.camp-energy {
+  margin-top: 24px;
+
+  &::before {
+    content: attr(data-value) "%";
+    font-size: 12px;
+    font-weight: 200;
+    line-height: 2;
+    text-align: center;
+    color: #fa4;
+    display: block;
+    width: 100%;
+  }
+
+  &-zero::before {
+    color: #666;
+  }
+}
+</style>
--- client/src/components/game-camps-status.vue
+++ client/src/components/game-camps-status.vue
@@ -1,7 +1,7 @@
 <script>
 import { NGrid, NGridItem, NProgress } from 'naive-ui';
 
-import GameCampBanner from '@components/game-camp-banner.vue';
+import GameCampStatus from '@components/game-camp-status.vue';
 import Flexbox from '@components/flexbox.vue';
 
 export default {
@@ -9,7 +9,7 @@ export default {
     NGrid,
     NGridItem,
     NProgress,
-    GameCampBanner,
+    GameCampStatus,
     Flexbox,
   },
   inject: ['game'],
@@ -23,23 +23,9 @@ export default {
       x-gap="8"
     >
       <n-grid-item>
-        <h4 class="camp-title">
-          Camp 1
-        </h4>
-        <game-camp-banner
-          :color="!game.current.team1.energy ? 'gray' : 'red'"
-        />
-        <n-progress
-          :percentage="game.current.team1.energy"
-          :data-value="game.current.team1.energy"
-          :show-indicator="false"
-          type="line"
-          color="#fa4"
-          :height="8"
-          :class="[
-            'camp-energy',
-            { 'camp-energy-zero': !game.current.team1.energy },
-          ]"
+        <game-camp-status
+          number="1"
+          :energy="game.current.team1.energy"
         />
       </n-grid-item>
 
@@ -51,23 +37,9 @@ export default {
       </n-grid-item>
 
       <n-grid-item>
-        <h4 class="camp-title">
-          Camp 2
-        </h4>
-        <game-camp-banner
-          :color="!game.current.team2.energy ? 'gray' : 'navy'"
-        />
-        <n-progress
-          :percentage="game.current.team2.energy"
-          :data-value="game.current.team2.energy"
-          :show-indicator="false"
-          type="line"
-          color="#fa4"
-          :height="8"
-          :class="[
-            'camp-energy',
-            { 'camp-energy-zero': !game.current.team2.energy },
-          ]"
+        <game-camp-status
+          number="2"
+          :energy="game.current.team2.energy"
         />
       </n-grid-item>
     </n-grid>
@@ -77,30 +49,6 @@ export default {
 <style lang="scss" scoped>
 .camps-status {
   padding: 24px;
-}
-.camp-title {
-  font-size: 20px;
-  font-weight: 200;
-  text-align: center;
-  margin-bottom: 12px;
-}
-.camp-energy {
-  margin-top: 24px;
-
-  &::before {
-    content: attr(data-value) "%";
-    font-size: 12px;
-    font-weight: 200;
-    line-height: 2;
-    text-align: center;
-    color: #fa4;
-    display: block;
-    width: 100%;
-  }
-
-  &-zero::before {
-    color: #666;
-  }
 }
 .versus {
   font-family: fantasy;
